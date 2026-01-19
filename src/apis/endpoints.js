05242f1bff4f12778a555d66c015b513b0ad8e3f
Goutam <goutam.kathuria@gmail.com>
2026-01-19 10:53:02 +0530

    .com build

--- src/apis/endpoints.js
+++ src/apis/endpoints.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "https://api.fivlia.in";
+export const API_BASE_URL = "https://api.fivlia.com";
+// export const API_BASE_URL = "https://api.fivlia.in";
 //  export const API_BASE_URL = "https://api.fivlia.co.in";
 // export const API_BASE_URL = "http://127.0.0.1:8080";
 // export const API_BASE_URL = "http://localhost:8080";
